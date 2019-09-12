0f93557f8a9f52f66a89262a7d52a99cc9d7c829
beatavg <beata.grothusen@gmail.com>
2019-09-12 12:01:56 +0200

    Steam VR 2.3.2

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour
+{
+    public SteamVR_Action_Boolean m_GrabAction = null;
+
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private FixedJoint m_Joint = null;
+
+    private Interactable m_CurrentInteractable = null;
+    public List<Interactable> m_ContactInteractables = new List<Interactable>();
+
+    private void Awake()
+    {
+        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+        m_Joint = GetComponent<FixedJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Down
+        if(m_GrabAction.GetLastStateDown(m_Pose.inputSource))
+        {
+            print(m_Pose.inputSource + " Trigger Down");
+            Pickup();
+        }
+        // Up
+        if (m_GrabAction.GetLastStateUp(m_Pose.inputSource))
+        {
+            print(m_Pose.inputSource + " Trigger Up");
+            Drop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Interactable"))
+            return;
+
+        m_ContactInteractables.Add(other.gameObject.GetComponent<Interactable>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Interactable"))
+            return;
+
+        m_ContactInteractables.Remove(other.gameObject.GetComponent<Interactable>());
+    }
+
+    public void Pickup()
+    {
+
+    }
+
+    public void Drop()
+    {
+
+    }
+
+    private Interactable GetNearestInteractable()
+    {
+        return null;
+    }
+
+
+}
---
+++ Assets/Scripts/ViveInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInput : MonoBehaviour
+{
+    //[SteamVR_DefaultAction("Squeeze")]
+
+    public SteamVR_Action_Single squeezeAction;
+
+    public SteamVR_Action_Vector2 touchPadAction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SteamVR_Actions._default.Teleport.GetStateUp(SteamVR_Input_Sources.Any))
+        {
+            print("Teleport down");
+        }
+
+        if (SteamVR_Actions._default.GrabPinch.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            print("Grab pinch up");
+        }
+
+        float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.Any);
+
+        if (triggerValue > 0.0f)
+        {
+            print(triggerValue);
+        }
+
+        Vector2 touchpadValue = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
+
+        if(touchpadValue != Vector2.zero)
+        {
+            print(touchpadValue);
+        }
+    }
+}
