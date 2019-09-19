043c5b0e452f2b1a197a4dcb7f01083ff1b4dd13
Beata von Grothusen <beatavg@n165-p167.eduroam.kth.se>
2019-09-19 16:07:50 +0200

    Revert "Revert "Working grabbing fish rod""
    This reverts commit 7f01ab0d81946a983f65c102cfb7fb38dfb61816.

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour
+{
+    public SteamVR_Action_Boolean m_GrabAction = null;
+
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private FixedJoint m_Joint = null;
+
+    private Interactable m_CurrentInteractable = null;
+    public List<Interactable> m_ContactInteractables = new List<Interactable>();
+
+    private void Awake()
+    {
+        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+        m_Joint = GetComponent<FixedJoint>();
+    }
+
+    private void Update()
+    {
+        // Down
+        if (m_GrabAction.GetStateDown(m_Pose.inputSource))
+        {
+            print(m_Pose.inputSource + " Trigger Down");
+            Pickup();
+        }
+
+        //Up
+        if (m_GrabAction.GetStateUp(m_Pose.inputSource))
+        {
+            print(m_Pose.inputSource + " Trigger Up");
+            Drop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Interactable"))
+            return;
+
+        m_ContactInteractables.Add(other.gameObject.GetComponent<Interactable>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Interactable"))
+            return;
+
+        m_ContactInteractables.Remove(other.gameObject.GetComponent<Interactable>());
+    }
+
+    private void Pickup()
+    {
+        //Get nearest
+        m_CurrentInteractable = GetNearestInteractable();
+
+        //Null check
+        if (!m_CurrentInteractable)
+            return;
+
+        //Already held, check
+        if (m_CurrentInteractable.m_ActiveHand)
+            m_CurrentInteractable.m_ActiveHand.Drop();
+
+        //Position
+        m_CurrentInteractable.transform.position = transform.position;
+
+        //Attach
+        Rigidbody targetBody = m_CurrentInteractable.GetComponent<Rigidbody>();
+        m_Joint.connectedBody = targetBody;
+
+        //Set active hand
+        m_CurrentInteractable.m_ActiveHand = this;
+    }
+
+    private void Drop()
+    {
+        //Null chech
+        if (!m_CurrentInteractable)
+            return;
+
+        //Apply velocity
+        Rigidbody targetBody = m_CurrentInteractable.GetComponent<Rigidbody>();
+        targetBody.velocity = m_Pose.GetVelocity();
+        targetBody.angularVelocity = m_Pose.GetAngularVelocity();
+
+        //Detch
+        m_Joint.connectedBody = null;
+
+        //Clear
+        m_CurrentInteractable.m_ActiveHand = null;
+        m_CurrentInteractable = null;
+    }
+
+    private Interactable GetNearestInteractable()
+    {
+        Interactable nearest = null;
+        float minDistance = float.MaxValue;
+        float distance = 0.0f;
+
+        foreach (Interactable interactable in m_ContactInteractables)
+        {
+            distance = (interactable.transform.position - transform.position).sqrMagnitude;
+
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearest = interactable;
+            }
+        }
+
+        return nearest;
+    }
+}
