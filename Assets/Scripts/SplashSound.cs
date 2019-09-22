4eb58911a3a65209bd348b9fd5b6a43a28a0df80
Beata von Grothusen <beatavg@Beatas-MacBook-Air.local>
2019-09-22 17:22:49 +0200

    added splashes to soundscape

---
+++ Assets/Scripts/SplashSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashSound : MonoBehaviour
+{
+
+    public AudioSource splash;
+
+    public int wait = 10;
+
+    bool keepPlaying = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SoundOut());
+        splash = GetComponent<AudioSource>();
+    }
+
+    IEnumerator SoundOut()
+     {
+         while (keepPlaying){
+             splash.Play();  
+             Debug.Log("SPLASH");
+             yield return new WaitForSeconds(wait);
+         }
+
+
+}
